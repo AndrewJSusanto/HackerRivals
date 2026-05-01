fb6da58f4c26a93aa802db4ab94cf95b837c6586
mikeslgoh <gohmichaelsl@gmail.com>
2026-05-01 16:29:52 -0700

    push Joce UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,6 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app/App.jsx";
-import Admin from "./pages/Admin.jsx";
-import { AuthProvider } from "./app/context/AuthContext";
-import "./styles/index.css";
 
-const isAdminRoute = window.location.pathname.startsWith("/admin");
+  import { createRoot } from "react-dom/client";
+  import App from "./app/App.jsx";
+  import "./styles/index.css";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {isAdminRoute ? (
-      <Admin />
-    ) : (
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    )}
-  </React.StrictMode>
-);
+  createRoot(document.getElementById("root")).render(<App />);
